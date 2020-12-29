b365204c7b4482181c551a5607d4cf31aa17d347
Jacqueline Nixon <jacquelinenixon@me.com>
2020-12-29 00:42:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function happyCustomHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountdown(days, holiday) {
-  `It's ${days} days until ${holiday}!`
+function holidayCountdown(holiday, days) {
+  return `It's ${days} days until ${holiday}!`
 }
